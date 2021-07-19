bff49ecd9612bb00395b8ad63141a57c0fdf6213
ibadhussain <hussainibad187@gmail.com>
2021-07-19 16:02:19 -0700

    updated file

--- script.js
+++ script.js
@@ -2,47 +2,28 @@ let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let header = document.querySelector('.header-2');
 
-menu.addEventListener('click', () =>{
+menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 });
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 
-    if(window.scrollY > 150){
+    if (window.scrollY > 20) {
         header.classList.add('active');
-    }else{
+    } else {
         header.classList.remove('active');
     }
 
 }
 
-let countDate = new Date('june 1, 2021 00:00:00').getTime();
-
-function CountDown(){
-
-    let now = new Date().getTime();
-    gap = countDate - now;
-
-    let second = 1000;
-    let minute = second * 60;
-    let hour = minute * 60;
-    let day = hour * 24;
-
-    let d = Math.floor(gap / (day));
-    let h = Math.floor((gap % (day)) / (hour));
-    let m = Math.floor((gap % (hour)) / (minute));
-    let s = Math.floor((gap % (minute)) / (second));
-
-    document.getElementById('day').innerText = d;
-    document.getElementById('hour').innerText = h;
-    document.getElementById('minute').innerText = m;
-    document.getElementById('second').innerText = s;
-
+function calc(n) {
+    var price = document.getElementsByClassName("item_price")[n].innerHTML;
+    var noTickets = document.getElementsByClassName("num")[n].value;
+    var total = parseFloat(price) * noTickets;
+    if (!isNaN(total))
+        document.getElementsByClassName("total")[n].innerHTML = total;
 }
 
-setInterval(function(){
-    CountDown();
-},1000)
